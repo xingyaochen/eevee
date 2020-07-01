dfc9e52cea55ceaadc40ed5223071fff22f55663
xingyaochen <xingyao314@gmail.com>
2020-06-30 22:52:10 -0700

    algo update

--- eevee_chord.R
+++ eevee_chord.R
@@ -4,8 +4,8 @@ library(chorddiag)
 library(htmlwidgets)
 
 # Create data
-m <- matrix(c(5, 0, 0, 1, 1, 0, 0, 0, 3,
-              0, 5, 1, 0, 1, 1, 3, 1, 0,
+m <- matrix(c(5, 0, 0, 0, 1, 0, 0, 0, 3,
+              0, 4, 1, 0, 1, 1, 3, 1, 0,
               0, 1, 4, 1, 1, 2, 2, 2, 0,
               0, 0, 1, 5, 0, 1, 1, 1, 1,
               1, 1, 1, 0, 5, 1, 1, 1, 1,
@@ -27,4 +27,4 @@ groupColors <- c('#C59161', '#7CCEE6', '#F8CE6A', '#EA824F', '#DCBFD5', '#4B5A5D
 p <- chorddiag(m, groupColors = groupColors, groupnamePadding = 10, showTicks = F)
 p
 
-saveWidget(p, file=paste0( getwd(), "/eevee_chord_interactive.html"))
+saveWidget(p, file=paste0( getwd(), "/index.html"))
