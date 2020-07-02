5638e04fe113797b3cca0c2b130c5aaadc71025d
xingyaochen <xingyao314@gmail.com>
2020-07-02 15:35:13 -0700

    yee

--- eevee.py
+++ eevee.py
@@ -1,4 +1,5 @@
 from itertools import chain, combinations
+import matplotlib.pyplot as plt
 
 VODKA = 1
 KALUA = 2
@@ -6,7 +7,7 @@ BAILYS = 3
 ICE_CREAM = 4
 CHOCOLATE_SYRUP = 5
 RUM = 6
-MALIBU = 7
+MALIBU = RUM
 BLUE_CURACAO = 8
 PINAPPLE_JUICE = 9
 SPRITE = 10
@@ -40,7 +41,7 @@ ingredient_map = {
     4: 'ICE_CREAM', # ['ICE_CREAM', 'CREAM'],
     5: 'CHOCOLATE_SYRUP',
     6: 'RUM',
-    7: 'MALIBU',
+    # 7: 'MALIBU',
     8: 'BLUE_CURACAO',
     9: 'PINAPPLE_JUICE',
     10: 'SODA_WATER', # ['SPRITE', 'COKE', 'SODA_WATER', 'GINGER_ALE'],
@@ -140,7 +141,8 @@ def make_ingredient_graph():
 ingredient_graph = make_ingredient_graph()
 
 
-def heuristic_solve():
+
+def heuristic_solve_dumb(num_drinks):
     evvee_list = list(evvee_graph.values())
     power_evvee_list = powerset(evvee_list)
     best_eevee_set = set()
@@ -148,10 +150,44 @@ def heuristic_solve():
     for eevee_list_set in power_evvee_list:
         if not eevee_list_set: continue
         # ingredient_interestion = set.intersection(*eevee_list_set)
+        if len(eevee_list_set) != num_drinks: continue
         ingredient_union = set.union(*eevee_list_set)
-        if (len(ingredient_union)) < len(best_ingredient_union) and len(eevee_list_set) > 3:
+        if (len(ingredient_union)) < len(best_ingredient_union):
             best_ingredient_union = ingredient_union
             best_eevee_set = eevee_list_set
+    return len(best_ingredient_union)
+
+
+
+def baseline_solve_dumb(num_drinks):
+    evvee_list = list(evvee_graph.values())
+    power_evvee_list = powerset(evvee_list)
+    best_eevee_set = set()
+    best_ingredient_union = set()
+    for eevee_list_set in power_evvee_list:
+        if not eevee_list_set: continue
+        if len(eevee_list_set) != num_drinks: continue
+        ingredient_union = set.union(*eevee_list_set)
+        if (len(ingredient_union)) > len(best_ingredient_union):
+            best_ingredient_union = ingredient_union
+            best_eevee_set = eevee_list_set
+    return len(best_ingredient_union)
+
+
+def heuristic_solve():
+    evvee_list = list(evvee_graph.values())
+    power_evvee_list = powerset(evvee_list)
+    best_eevee_set = set()
+    best_ingredient_union = set(ingredient_list)
+    best_ratio = float('inf')
+    for eevee_list_set in power_evvee_list:
+        if not eevee_list_set: continue
+        ingredient_union = set.union(*eevee_list_set)
+        if  len(ingredient_union) / len(eevee_list_set) < best_ratio and len(eevee_list_set) > 3:
+            best_eevee_set = eevee_list_set
+            best_ratio = len(ingredient_union) / len(eevee_list_set)
+            best_ingredient_union = ingredient_union
+    print('best_ratio: {}/{}'.format(len(best_ingredient_union), len(best_eevee_set)))
     print('Shopping List')
     print([ingredient_map[ing] for ing in best_ingredient_union])
     eevees = find_eevee(best_eevee_set)
@@ -167,5 +203,41 @@ for i, x in enumerate(evvee_graph.values()):
         eevee_matrix[i][j] = dist
 
 
+
+
+# Create two subplots and unpack the output array immediately
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+plt.xlabel('number of eevee drinks')
+
+# heuristic_solve()
+x = [i for i in range(1, len(evvee_graph.values()) + 1)]
+# x = [1]
+y1 = []
+y2 = []
+for i in x:
+    y1.append(heuristic_solve_dumb(i))
+    y2.append(baseline_solve_dumb(i))
+
+ax1.plot(x, y1, 'r-', x, y2, 'b-')
+ax1.legend(['min', 'max'])
+ax1.set_ylabel('number of ingredients')
+ax1.grid()
+
+# heuristic_solve()
+x = [i for i in range(1, len(evvee_graph.values()) + 1)]
+# x = [1]
+y1 = []
+y2 = []
+for i in x:
+    y1.append(heuristic_solve_dumb(i)/i)
+    y2.append(baseline_solve_dumb(i)/i)
+
+# plt.grid()
+ax2.plot(x, y1, 'r-', x, y2, 'b-')
+ax2.legend(['min', 'max'])
+ax2.set_ylabel('avg ingredients / drink')
+ax2.grid()
+plt.savefig('result.png')
+plt.close()
 
 heuristic_solve()
